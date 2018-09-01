ca4b2132422bca6f6bbf5e93f4e96292e46eb5b9
LeMemeOfficer <42116533+LeMemeOfficer@users.noreply.github.com>
2018-09-01 13:49:25 +0200

    Added baclground and ground image

--- ground.js
+++ ground.js
@@ -1,3 +1,10 @@
+//load images
+var image_carton = new Image();
+image_carton.src = "images/patterns/ground_carton2.png";
+
+var image_carton_ripped = new Image();
+image_carton_ripped.src = "images/patterns/ground_carton_ripped2.png";
+
 function ground()
 {
   this.groundMatrix = [];
@@ -7,20 +14,29 @@ function ground()
     this.canvas.getContext("2d").clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
 
+
   this.drawGround = function()
   {
     ctx = groundCanvas.context;
-    ctx.fillStyle = "green";
+
+    var pattern = ctx.createPattern(image_carton, "repeat");
+    var pattern_outline = ctx.createPattern(image_carton_ripped, "repeat");
+
+    ctx.fillStyle = pattern;
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = pattern_outline;
 
     ctx.beginPath();
-    ctx.moveTo(groundCanvas.canvas.width, groundCanvas.canvas.height);
-    ctx.lineTo(0, groundCanvas.canvas.height);
+    ctx.moveTo(groundCanvas.canvas.width, groundCanvas.canvas.height + 10);
+    ctx.lineTo(-10, groundCanvas.canvas.height + 10);
+    ctx.lineTo(-10, groundCanvas.canvas.height - (this.groundMatrix[0] - 1))
     for(i = 0; i < this.groundMatrix.length; i++)
     {
       ctx.lineTo(i * 2, groundCanvas.canvas.height - (this.groundMatrix[i] - 1));
     }
 
     ctx.fill();
+    ctx.stroke();
   }
 
   this.createGroundMatrix = function()
