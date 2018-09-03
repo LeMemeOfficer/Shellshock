ef25eaab7c82eede9e8993579da9f7c4308288cd
LeMemeOfficer <42116533+LeMemeOfficer@users.noreply.github.com>
2018-09-03 13:48:13 +0200

    Added multiple explosion pictures

--- explosion.js
+++ explosion.js
@@ -1,3 +1,13 @@
+//load images
+var image_explosion1 = new Image();
+image_explosion1.src = "images/assets/explosion1.png";
+
+var image_explosion2 = new Image();
+image_explosion2.src = "images/assets/explosion2.png";
+
+var image_explosion3 = new Image();
+image_explosion3.src = "images/assets/explosion3.png";
+
 function explosionObject(xPos, yPos)
 {
   this.id = explosionId.getNextId();
@@ -5,6 +15,8 @@ function explosionObject(xPos, yPos)
   //positionOfExplosion
   this.xPos = xPos;
   this.yPos = yPos;
+
+  this.explosionType = Math.floor(Math.random() * 3);
 
   this.existenceTimeCounter = 0;
 
@@ -23,7 +35,21 @@ function explosionObject(xPos, yPos)
 
   this.drawExplosion = function()
   {
-    ctx = myGameArea.context;
-    ctx.drawImage(image_explosion1, xPos - Math.floor(image_explosion1.width / 2), yPos - Math.floor(image_explosion1.height / 2));
+    switch (this.explosionType) {
+      case 0:
+        ctx = myGameArea.context;
+        ctx.drawImage(image_explosion1, xPos - Math.floor(image_explosion1.width / 2), yPos - Math.floor(image_explosion1.height / 2));
+        break;
+      case 1:
+        ctx = myGameArea.context;
+        ctx.drawImage(image_explosion2, xPos - Math.floor(image_explosion2.width / 2), yPos - Math.floor(image_explosion2.height / 2));
+        break;
+      case 2:
+        ctx = myGameArea.context;
+        ctx.drawImage(image_explosion3, xPos - Math.floor(image_explosion3.width / 2), yPos - Math.floor(image_explosion3.height / 2));
+        break;
+      default:
+
+    }
   }
 }
--- helpers.js
+++ helpers.js
@@ -1,7 +1,3 @@
-//load images
-var image_explosion1 = new Image();
-image_explosion1.src = "images/assets/explosion1.png";
-
 function inBetween(variable, min, max)
 {
   if(variable >= min && variable <= max)
@@ -40,10 +36,4 @@ function drawCircle(ctx, xPos, yPos, radius, color)
   ctx.beginPath();
   ctx.arc(xPos, yPos, radius, 0,2*Math.PI);
   ctx.fill();
-}
-
-function drawExplosion(xPos, yPos)
-{
-  ctx = myGameArea.context;
-  ctx.drawImage(image_explosion1, xPos - Math.floor(image_explosion1.width / 2), yPos - Math.floor(image_explosion1.height / 2));
 }
