4abbcb7741195108891e287d5779dafad5cce82d
LeMemeOfficer <42116533+LeMemeOfficer@users.noreply.github.com>
2018-09-03 19:35:10 +0200

    Added shoot intensity cone

--- calculations.js
+++ calculations.js
@@ -3,6 +3,11 @@ function getDistance(p1, p2)
   return Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2));
 }
 
+function getAngle(p1, p2)
+{
+  return (Math.atan2(p1[1] - p2[1], p1[0] - p2[0]) * 180 / Math.PI)
+}
+
 function inBetween(variable, min, max)
 {
   if(variable >= min && variable <= max)
--- gameLogicHelpers.js
+++ gameLogicHelpers.js
@@ -1,3 +1,11 @@
+//--------------------PARAMETER--------------------
+var INTENSITY_MODIFIER = 3.2;
+var COLOR_MODIFIER_DISTANCE = 300;
+var CONE_OFFSET = 20;
+var CONE_LENGTH = 100;
+var CONE_WIDTH = 10;
+//--------------------PARAMETER--------------------
+
 function getNextPlayer()
 {
   players[activePlayer].movementCounter = 0
@@ -20,13 +28,35 @@ function counter()
   }
 }
 
-function drawBulletPath(player)
+function drawShootSpeedCone(player)
 {
+  var mainAngele = getAngle([player.x, player.y], [mousePosition[0], mousePosition[1]]);
+  var distance = getDistance(getNextEdge([player.x, player.y], CONE_OFFSET, mainAngele, 2), [mousePosition[0], mousePosition[1]]);
+
+  var pos1 = getNextEdge([player.x, player.y], CONE_OFFSET, mainAngele, 2);
+  var pos2 = getNextEdge(pos1, distance / INTENSITY_MODIFIER, mainAngele + CONE_WIDTH, 2);
+  var pos3 = getNextEdge(pos1, distance / INTENSITY_MODIFIER, mainAngele - CONE_WIDTH, 2);
+
+  var colorModifier = distance / COLOR_MODIFIER_DISTANCE * 255;
+
   ctx = myGameArea.context;
-  ctx.strokeStyle = "gray";
+
+  ctx.fillStyle = "rgb(" + colorModifier + ", 0," + (255 - colorModifier) + ")";
 
   ctx.beginPath();
-  ctx.moveTo(player.x, player.y);
-  ctx.lineTo(mousePosition[0], mousePosition[1]);
+  ctx.moveTo(pos1[0], pos1[1]);
+  ctx.lineTo(pos2[0], pos2[1]);
+  ctx.lineTo(pos3[0], pos3[1]);
+  ctx.fill();
+
+  pos2 = getNextEdge(pos1, CONE_LENGTH, mainAngele + CONE_WIDTH, 2);
+  pos3 = getNextEdge(pos1, CONE_LENGTH, mainAngele - CONE_WIDTH, 2);
+
+  ctx.fillStyle = "black"
+  ctx.beginPath();
+  ctx.moveTo(pos1[0], pos1[1]);
+  ctx.lineTo(pos2[0], pos2[1]);
+  ctx.lineTo(pos3[0], pos3[1]);
+  ctx.lineTo(pos1[0], pos1[1]);
   ctx.stroke();
 }
